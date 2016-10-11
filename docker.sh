8c19c571619ce1250db3243535f9cdec81ee5fbc
Damodaran, Vibin <Vibin.Damodaran@hmhco.com>
2016-10-11 09:57:19 -0400

    comment the start scripts

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 cp /home/centos/config-prj/docker.repo /etc/yum.repos.d/
 yum install docker-engine
-systemctl enable docker.service
-systemctl start docker
+#systemctl enable docker.service
+#systemctl start docker
